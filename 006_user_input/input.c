08eb2614a50cb600710b02f8e86eff692cfcf5db
SSR <iamssrobinson@gmail.com>
2023-03-13 02:44:02 +0000

    Wrote a program to store two user generated int values using the scanf() function and then calculated and displayed the sum total.

--- 006_user_input/input.c
+++ 006_user_input/input.c
@@ -1,1 +1,21 @@
 // Write a C program to compute the sum of two integer values taken via user input, return the sum total and display the result.
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+	int firstNumber, secondNumber, total;
+	
+	printf("\nPlease enter your first number: \n");
+	scanf("%d", &firstNumber);
+
+	printf("Please enter your second number: \n");
+	scanf("%d", &secondNumber);
+
+	total = firstNumber + secondNumber;
+
+	printf("\n%d + %d = %d\n\n", firstNumber, secondNumber, total);
+
+
+	return 0;
+}
