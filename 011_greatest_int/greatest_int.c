579ff6e5d8dc85a53632f4066fa6b7dfbbdf45f7
SSR <iamssrobinson@gmail.com>
2023-03-18 05:02:11 +0000

    Wrote a progam to find the greatest of 3 user defined numbers.

---
+++ 011_greatest_int/greatest_int.c
@@ -0,0 +1,28 @@
+// Find the greatest of 3 numbers
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+	int num1, num2, num3;
+	printf("\nPlease enter 3 numbers separated by a space: \n");
+	scanf("%d %d %d", &num1, &num2, &num3);
+
+	if(num1 > num2){
+		if(num1 > num3){
+			printf("\nNum1 is greatest!\n");
+		} else {
+				printf("\nNum3 is greatest!\n");
+		}
+	} else {
+			if(num2 > num3){
+				printf("\nNum2 is greatest!\n");
+			}
+			else{
+					printf("\nNum3 is greatest!\n");
+			}
+	} 
+
+	
+	return 0;
+}
