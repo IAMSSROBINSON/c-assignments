5c69f013028bddf2e972816f78bb634734c64d9d
SSR <iamssrobinson@gmail.com>
2023-03-19 12:49:51 +0000

    Wrote a program to determine whether a given number is positive, negative or 0.

---
+++ 012_positive_negative/positive_negative.c
@@ -0,0 +1,22 @@
+// Write a program to check if a given number is positive, negative or equal to 0
+
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+	int number;
+
+	printf("\nPlease enter a number: \n");
+	scanf("%d", &number);
+
+	if(number > 0){
+		printf("\nThe number you entered is positive!\n");
+	} else if(number < 0){
+		printf("\nThe number you entered is negative!\n");
+	} else {
+		printf("\nThe number you entered is equal to 0!\n");
+	}
+
+
+	return 0;
+}
