a138cb00e39b34966dc78db92050e2e59c7a7ab7
SSR <iamssrobinson@gmail.com>
2023-03-22 02:48:01 +0000

    Wrote a program to read the length of a string and display to output stream.

---
+++ 015_string_length/string_length.c
@@ -0,0 +1,17 @@
+// Write a program to find the length of a given string.
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main (int argc, char *argv[]) {
+
+	char string[] = "Stephen";
+
+	printf("\nLength of string is: %lu\n", strlen(string));
+
+
+	return 0;
+}
+
