53fa1755e657cbadd909a870ec12d2347a120bad
SSR <iamssrobinson@gmail.com>
2023-03-30 04:03:55 +0100

    Used addressOf operator to send address of variables to a function, received them as pointers and accessed values with pointer to variable operator to enable explicit value swap at the same memory address.

--- 023_pointers_swap/pointer_swap.c
+++ 023_pointers_swap/pointer_swap.c
@@ -1,2 +1,33 @@
-// Use pointers in a function to swap the value of two numbers
+// Use pointers in a function to swap the values of two numbers
+
+#include <stdio.h>
+
+void swap (int *a, int *b);
+
+
+int main (int argc, char *argv[]) {
+
+  int a = 100;
+  int b = 200;
+
+  printf("\nVALUES BEFORE SWAP\n");
+  printf("A: %d\tAddress: %p\nB: %d\tAddress: %p\n\n", a, &a, b, &b);
+
+  swap(&a, &b);
+
+
+  return 0;
+}
+
+void swap (int *a, int *b) {
+
+  int c;
+  c = *a;
+  *a = *b;
+  *b = c;
+
+  printf("\nVALUES AFTER SWAP\n");
+  printf("A: %d\tAddress: %p\nB: %d\tAddress: %p\n\n", *a, a, *b, b);
+
+}
 
