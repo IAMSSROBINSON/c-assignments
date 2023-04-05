db6e23bfe105a84d9ac85f29bd0da5fefe32100b
SSR <iamssrobinson@gmail.com>
2023-04-05 19:11:35 +0100

    Used fputc to write a character to an external file.Used w write mode of fopen function to create the file if not exists prior to writing to it.

--- 029_write_char_to_file/writeChar.c
+++ 029_write_char_to_file/writeChar.c
@@ -1,1 +1,30 @@
 // Write a program to write a character to an external file.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void writeChar (char *character);
+
+
+int main (int argc, char *argv[]) {
+
+  char character = 'A';
+  writeChar(&character);
+  
+
+  return 0;
+}
+
+
+void writeChar (char *character) {
+
+  FILE *file_ptr;
+  file_ptr = fopen("data.txt", "w");
+
+  if (file_ptr == NULL){
+    printf("\nFile could not be opened!\n");
+  }
+  printf("\nFile opened!\n");
+
+  fputc(*character, file_ptr);
+}
