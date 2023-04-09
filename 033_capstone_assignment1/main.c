5997196d3fc5b6577402d4527019e96aea50d904
SSR <iamssrobinson@gmail.com>
2023-04-09 19:38:20 +0100

    capstone_assignment1 'Players'.

--- 033_capstone_assignment1/main.c
+++ 033_capstone_assignment1/main.c
@@ -2,24 +2,22 @@
 #include <stdlib.h>
 #include "players.h"
 
-int main () {
-	// declare variables
-	int menuSelection;
-	int numberOfPlayers;
-	CREATEPLAYER *arrayOfPlayers;
-	int menu2selection;
+int main(int argc, char *argv[])
+{
+  // declare variables
+  int menuSelection;
+  int numberOfPlayers;
+  CREATEPLAYER *arrayOfPlayers;
+  int menu2selection;
 
+  menuSelection = menu1();
+  numberOfPlayers = validation(menuSelection);
+  arrayOfPlayers = allocateMemory(numberOfPlayers);
+  arrayOfPlayers = populateArray(arrayOfPlayers, numberOfPlayers);
+  printArray(arrayOfPlayers, numberOfPlayers);
+  menu2selection = menu2();
+  validation2(arrayOfPlayers, numberOfPlayers, menu2selection);
 
-	menuSelection =  menu1();
-	numberOfPlayers = validation(menuSelection);
-	arrayOfPlayers = allocateMemory(numberOfPlayers);
-	arrayOfPlayers = populateArray(arrayOfPlayers, numberOfPlayers);
-	printArray(arrayOfPlayers, numberOfPlayers);
-	menu2selection = menu2();
-	validation2(arrayOfPlayers, numberOfPlayers, menu2selection);
-
-
-	free(arrayOfPlayers);
-	return 0;
+  free(arrayOfPlayers);
+  return 0;
 }
-
--- 033_capstone_assignment1/players.h
+++ 033_capstone_assignment1/players.h
@@ -1,10 +1,11 @@
 #ifndef PLAYERS_H
 #define PLAYERS_H
 
-typedef struct Player {
-	int id;
-	char name[20];
-	int age;
+typedef struct Player
+{
+  int id;
+  char name[20];
+  int age;
 } CREATEPLAYER;
 
 int menu1();
@@ -15,11 +16,8 @@ void printArray(CREATEPLAYER *arrayOfPlayers, int numberOfPlayers);
 int menu2();
 void validation2(CREATEPLAYER *arrayOfPlayers, int numberOfPlayers, int menu2selection);
 void allocateMorePlayers(CREATEPLAYER *arrayOfPlayers, int numberOfPlayers, int menu2_addPlayers);
-void addPlayerDetails (CREATEPLAYER *arrayOfPlayers, int numberOfPlayers, int menu2_addPlayers);
+void addPlayerDetails(CREATEPLAYER *arrayOfPlayers, int numberOfPlayers, int menu2_addPlayers);
 void deleteById(CREATEPLAYER *arrayOfPlayers, int numberOfPlayers, int searchId);
 void searchById(CREATEPLAYER *arrayOfPlayers, int numberOfPlayers, int searchId);
-
-
-
 
 #endif
